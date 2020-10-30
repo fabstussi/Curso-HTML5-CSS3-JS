406e1514a3c07df2aa88574cdae451d9cfd78634
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2020-10-30 13:49:43 -0300

    Arrumando o código

--- exercicios/ex011.js
+++ exercicios/ex011.js
@@ -3,27 +3,27 @@ function contar() {
   var contF = document.getElementById('txtF')
   var contP = document.getElementById('txtP')
   var res = document.getElementById('resultado')
-  res.innerText = ''
+  res.innerText = 'Contando: '
   if (contI.value.length == 0 || contF.value.length == 0 || contP.value.length == 0) {
-    alert('Por favor verifique se deixou alguma caixa de texto fazia')
     res.innerText += 'Não foi possível iniciar a contagem'
+    alert('Por favor verifique se deixou alguma caixa de texto fazia')
   } else {
     var inicio = Number(contI.value)
     var final = Number(contF.value)
     var passo = Number(contP.value)
-    if (passo == 0) {
-      alert('O passo não pode ser igual a 0, será usado o valor 1')
+    if (passo <= 0) {
+      alert('O passo invalido, será usado o valor 1')
       passo = 1
     }
     if (inicio < final) {
       for (var c = inicio; c <= final; c += passo) {
-        res.innerText += ` ${c} -> `
+        res.innerText += ` ${c} \u{1F449} `
       }
     } else {
       for (var c = inicio; c >= final; c -= passo) {
-        res.innerText += ` ${c} -> `
+        res.innerText += ` ${c} \u{1F449} `
       }
     }
-    res.innerText += ' Fim'
+    res.innerText += ' \u{1F3C1}'
   }
 }
--- exercicios/ex012.js
+++ exercicios/ex012.js
@@ -1,9 +1,13 @@
 function gerar() {
-    var num = document.getElementById('txtN')
-    var res = document.getElementById('res')
-    res.innerHTML = ''
-    var n = Number(num.value)
+  var num = document.getElementById('txtN')
+  var res = document.getElementById('res')
+  res.innerHTML = ''
+  var n = Number(num.value)
+  if (num.value.length == 0) {
+    alert('A caixa de texto não pode ficar vazia!')
+  } else {
     for (var mult = 1; mult <= 10; mult++) {
-        res.innerHTML += `${mult} x ${n} = ${mult*n}<br />`
+      res.innerHTML += `${mult} x ${n} = ${mult * n}<br />`
     }
+  }
 }
