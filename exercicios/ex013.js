d977b311714ae12548bc4cb7053dac7df538c316
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2020-11-04 21:43:21 -0300

    Criado o exercício 013 com JS alternativo
    Criado o exercício, porem coloquei um JS com as formas mais simplificadas e outra com JS com loops

---
+++ exercicios/ex013.js
@@ -0,0 +1,82 @@
+let vetor = []
+
+function preencheTela(selecionado, primo, parimp, maior, menor, soma, média) {
+  let dados = document.getElementById('dir')
+
+  dados.innerHTML = ''
+  if (selecionado != undefined) {
+    dados.innerHTML = `<p>O número selecionado foi ${selecionado}</p>`
+    dados.innerHTML += `<p>O número ${selecionado} ele ${primo} um número primo</p>`
+    dados.innerHTML += `<p>O número ${selecionado} é ${parimp} </p>`
+  }
+  dados.innerHTML += `<p>O maior número digitado foi o ${maior}</p>`
+  dados.innerHTML += `<p>O menor número digitado foi o ${menor}</p>`
+  dados.innerHTML += `<p>A soma dos números é ${soma}</p>`
+  dados.innerHTML += `<p>A média entre os números é ${média}</p>`
+}
+
+function ePrimo(valor) {
+  if (valor == 1) {
+    return 'não é'
+  } else {
+    let cont = 1
+    for (let i = 2; i <= valor; i++) {
+      if (valor % i == 0) {
+        cont++
+      }
+    }
+    if (cont != 2) {
+      return 'não é'
+    } else {
+      return 'é '
+    }
+  }
+}
+
+function manipulaVetor(valor) {
+  let dados = document.getElementById('lst')
+  let item = document.createElement('option')
+  if (vetor.length == 0) {
+    dados.innerHTML = ''
+    vetor.push(valor)
+    item.text = `O valor ${valor} foi adicionado`
+    item.value = valor
+    dados.appendChild(item)
+  } else if (vetor.indexOf(valor) == -1) {
+    vetor.push(valor)
+    item.text = `O valor ${valor} foi adicionado`
+    item.value = valor
+    dados.appendChild(item)
+  } else {
+    alert(`O número ${valor} já foi adicionado.`)
+  }
+  let maior = Math.max.apply(null, vetor)
+  let menor = Math.min.apply(null, vetor)
+  let soma = vetor.reduce(function (somador, n) { return somador + n })
+  let media = vetor.reduce(function (somador, n) { return somador + n }) / vetor.length
+  preencheTela(undefined, 0, 0, maior, menor, soma, media)
+}
+
+function adicionar() {
+  let n = document.getElementById('num')
+  let valor = Number(n.value)
+  if (n.length == 0) {
+    alert('[ERRO] Caixa de texto não pode ficar vazia')
+  } else if (valor < 0 || valor > 100) {
+    alert('[ERRO] Número digitado não esta entre 0 e 100')
+  } else {
+    manipulaVetor(valor)
+  }
+  n.innerHTML = ''
+}
+
+function verificar() {
+  let dados = document.getElementById('lst')
+  let primo = ePrimo(dados.value)
+  let parImp = dados.value % 2 == 0 ? 'par' : 'impar'
+  let maior = Math.max.apply(null, vetor)
+  let menor = Math.min.apply(null, vetor)
+  let soma = vetor.reduce(function (somador, n) { return somador + n })
+  let media = vetor.reduce(function (somador, n) { return somador + n }) / vetor.length
+  preencheTela(dados.value, primo, parImp, maior, menor, soma, media)
+}
---
+++ exercicios/ex013a.js
@@ -0,0 +1,119 @@
+let vetor = []
+
+function preencheTela(selecionado, primo, parImp, maior, menor, soma, média) {
+  let dados = document.getElementById('dir')
+
+  dados.innerHTML = ''
+  if (selecionado != undefined) {
+    dados.innerHTML = `<p>O número selecionado foi ${selecionado}</p>`
+    dados.innerHTML += `<p>O número ${selecionado} ele ${primo} um número primo</p>`
+    dados.innerHTML += `<p>O número ${selecionado} é ${parImp} </p>`
+  }
+  dados.innerHTML += `<p>O maior número digitado foi o ${maior}</p>`
+  dados.innerHTML += `<p>O menor número digitado foi o ${menor}</p>`
+  dados.innerHTML += `<p>A soma dos números é ${soma}</p>`
+  dados.innerHTML += `<p>A média entre os números é ${média}</p>`
+}
+
+function acharMenor() {
+  let menor = vetor[0]
+  for (let n in vetor) {
+    if (vetor[n] < menor) {
+      menor = vetor[n]
+    }
+  }
+  return menor
+}
+
+function acharMaior() {
+  let maior = vetor[0]
+  for (let n in vetor) {
+    if (vetor[n] > maior) {
+      maior = vetor[n]
+    }
+  }
+  return maior
+}
+
+function somaTudo() {
+  let soma = 0
+  for (let n in vetor) {
+    soma += vetor[n]
+  }
+  return soma
+}
+
+function achaMedia() {
+  return somaTudo() / vetor.length
+}
+
+function ePrimo(valor) {
+  if (valor == 1) {
+    return 'não é'
+  } else {
+    let cont = 1
+    for (let i = 2; i <= valor; i++) {
+      if (valor % i == 0) {
+        cont++
+      }
+    }
+    console.log(cont)
+    if (cont != 2) {
+      return 'não é'
+    } else {
+      return 'é '
+    }
+  }
+}
+
+function parImpar(valor) {
+  return valor % 2 == 0 ? 'par' : 'impar'
+}
+
+function manipulaVetor(valor) {
+  let dados = document.getElementById('lst')
+  let item = document.createElement('option')
+  if (vetor.length == 0) {
+    dados.innerHTML = ''
+    vetor.push(valor)
+    item.text = `O valor ${valor} foi adicionado`
+    item.value = valor
+    dados.appendChild(item)
+  } else if (vetor.indexOf(valor) == -1) {
+    vetor.push(valor)
+    item.text = `O valor ${valor} foi adicionado`
+    item.value = valor
+    dados.appendChild(item)
+  } else {
+    alert(`O número ${valor} já foi adicionado.`)
+  }
+  let menor = acharMenor()
+  let maior = acharMaior()
+  let soma = somaTudo()
+  let media = achaMedia()
+  preencheTela(undefined, 0, 0, maior, menor, soma, media)
+}
+
+function adicionar() {
+  let n = document.getElementById('num')
+  let valor = Number(n.value)
+  if (n.length == 0) {
+    alert('[ERRO] Caixa de texto não pode ficar vazia')
+  } else if (valor < 0 || valor > 100) {
+    alert('[ERRO] Número digitado não esta entre 0 e 100')
+  } else {
+    manipulaVetor(valor)
+  }
+  n.innerHTML = ''
+}
+
+function verificar() {
+  let dados = document.getElementById('lst')
+  let primo = ePrimo(dados.value)
+  let parImp = parImpar(dados.value)
+  let menor = acharMenor()
+  let maior = acharMaior()
+  let soma = somaTudo()
+  let media = achaMedia()
+  preencheTela(dados.value, primo, parImp, maior, menor, soma, media)
+}
